ee23cb051739402e2c76e8faeef42d36ef4f04ee
diwakarbhatt68 <diwakarbhatt68@gmail.com>
2016-12-20 14:28:38 +0530

    Finished Digit problem

--- src/com/company/general/DigitProblem.java
+++ src/com/company/general/DigitProblem.java
@@ -1,5 +1,28 @@
 package com.company.general;
 
-public class DigitProblem {
+import java.util.Scanner;
 
+public class DigitProblem {
+    static int count=0;
+    public static void main(String args[] ) throws Exception {
+
+        Scanner sc=new Scanner(System.in);
+        String digit_change=sc.nextLine();
+        String s_digit=digit_change.split(" ")[0];
+        int temp=digit_change.split(" ")[0].length();
+        int[] digit=new int[temp];
+        long change=Long.parseLong(digit_change.split(" ")[1]);
+        for(int i=0;i<temp;i++){
+            digit[i]=s_digit.charAt(i)-'0';
+        }
+        for(int i=0;i<temp;i++){
+            if(digit[i]<9){
+                if(count<change)
+                    digit[i]=9;
+                count+=1;
+            }
+        }
+        for(int temp1:digit)
+            System.out.print(temp1);
+    }
 }
